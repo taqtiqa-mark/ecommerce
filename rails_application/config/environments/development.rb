17af6c4ec6f8ee62b6ea42bf687e74694bb5930a
Mark Van de Vyver <mark@taqtiqa.com>
2022-05-18 14:04:38 +1000

    Evolve[Core]: Update Rails application
    Signed-off-by: Mark Van de Vyver <mark@taqtiqa.com>

--- rails_application/config/environments/development.rb
+++ rails_application/config/environments/development.rb
@@ -14,9 +14,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Enable server timing
+  config.server_timing = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join("tmp", "caching-dev.txt").exist?
+  if Rails.root.join("tmp","caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
@@ -63,12 +66,13 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
   config.hosts << ".test"
 
   # --- Ecommerce ---
-  config.number_generator = ->{ Ordering::NumberGenerator.new }
+  config.number_generator = -> { Ordering::NumberGenerator.new }
   config.payment_gateway = -> { @gateway ||= Payments::FakeGateway.new }
 end
